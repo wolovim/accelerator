31feb126968df76972b6cb45c705ceb46e8520f3
Marc Garreau <marcdgarreau@gmail.com>
2017-11-19 22:08:44 -0700

    Upgrades deps

--- src/index.js
+++ src/index.js
@@ -2,18 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
-
-// react-md utilizes Google's material icons
+import createBrowserHistory from 'history/createBrowserHistory';
 import WebFont from 'webfontloader';
-WebFont.load({ google: { families: ['Material Icons'] } });
 
 import App from './containers/App';
 import configureStore from './store/configureStore';
-
 import './assets/stylesheets/index.scss';
 
+// react-md utilizes Google's material icons
+WebFont.load({ google: { families: ['Material Icons'] } });
+
 // the same history instance is required in the store and ConnectedRouter
-import createBrowserHistory from 'history/createBrowserHistory';
 const history = createBrowserHistory();
 const store = configureStore(history);
 
